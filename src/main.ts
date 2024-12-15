6df398e419e93cc80013ca2dc8e57127a66ac430
Ruf141 <mad0908141@gmail.com>
2024-12-15 23:50:40 +0900

    import prisma modules

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValitatonPipe } from '@nestjs/common';
+import { Request } from 'express';
+import * as cookieParser from 'cookie-parser';
+import * as csurf from 'csurf';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3005);
+  app.useGlobalPipes(new ValitatonPipe({ whilelist: true }));
+  app.enableCors({
+    credentials: true,
+    origin: ['http://localhost:3000'],
+  });
+  app.use(cookieParser());
+  await app.listen(3005);
 }
 bootstrap();
